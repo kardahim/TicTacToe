9f65530cffe1a57f55967a4561dda68baeb5b3f8
Paweł Ciszewski <kardahim@gmail.com>
2021-07-12 08:01:29 +0200

    add function showWinner

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,11 @@ for (var i = 0; i < area.length; i++) {
     area[i].addEventListener("click", game);
 }
 
+document.getElementById("result").addEventListener("click", () => {
+    document.getElementById("result").style.display = "none";
+    reset();
+})
+
 function game() {
     draw(this.id);
     winner();
@@ -52,73 +57,73 @@ function winner() {
     // horizontal
     if (c0 === "X" && c1 === "X" && c2 === "X") {
         horizontalLine(0, 1, 2);
-        alert("X wins");
+        showWinner(1);
     }
     else if (c3 === "X" && c4 === "X" && c5 === "X") {
         horizontalLine(3, 4, 5);
-        alert("X wins");
+        showWinner(1);
     }
     else if (c6 === "X" && c7 === "X" && c8 === "X") {
         horizontalLine(6, 7, 8);
-        alert("X wins");
+        showWinner(1);
     }
     else if (c0 === "O" && c1 === "O" && c2 === "O") {
         horizontalLine(0, 1, 2);
-        alert("O wins");
+        showWinner(2);
     }
     else if (c3 === "O" && c4 === "O" && c5 === "O") {
         horizontalLine(3, 4, 5);
-        alert("O wins");
+        showWinner(2);
     }
     else if (c6 === "O" && c7 === "O" && c8 === "O") {
         horizontalLine(6, 7, 8);
-        alert("O wins");
+        showWinner(2);
     }
     // vertical
     else if (c0 === "X" && c3 === "X" && c6 === "X") {
         verticalLine(0, 3, 6);
-        alert("X wins");
+        showWinner(1);
     }
     else if (c1 === "X" && c4 === "X" && c7 === "X") {
         verticalLine(1, 4, 7);
-        alert("X wins");
+        showWinner(1);
     }
     else if (c2 === "X" && c5 === "X" && c8 === "X") {
         verticalLine(2, 5, 8);
-        alert("X wins");
+        showWinner(1);
     }
     else if (c0 === "O" && c3 === "O" && c6 === "O") {
         verticalLine(0, 3, 6);
-        alert("O wins");
+        showWinner(2);
     }
     else if (c1 === "O" && c4 === "O" && c7 === "O") {
         verticalLine(1, 4, 7);
-        alert("O wins");
+        showWinner(2);
     }
     else if (c2 === "O" && c5 === "O" && c8 === "O") {
         verticalLine(2, 5, 8);
-        alert("O wins");
+        showWinner(2);
     }
     // diagonal left
     else if (c0 === "X" && c4 === "X" && c8 === "X") {
         diagonalLine(0, 4, 8, true);
-        alert("X wins");
+        showWinner(1);
     }
     else if (c0 === "O" && c4 === "O" && c8 === "O") {
         diagonalLine(0, 4, 8, true);
-        alert("O wins");
+        showWinner(2);
     }
     else if (c2 === "X" && c4 === "X" && c6 === "X") {
         diagonalLine(2, 4, 6, false);
-        alert("X wins");
+        showWinner(1);
     }
     else if (c2 === "O" && c4 === "O" && c6 === "O") {
         diagonalLine(2, 4, 6, false);
-        alert("O wins");
+        showWinner(2);
     }
     // draw
     else if (c0 != "" && c1 != "" && c2 != "" && c3 != "" && c4 != "" && c5 != "" && c6 != "" && c7 != "" && c8 != "") {
-        alert("Draw");
+        showWinner(0);
     }
 }
 
@@ -174,4 +179,47 @@ function diagonalLine(f1, f2, f3, f4) {
         divider[f3].style.transform = "rotate(-45deg)";
     }
 
+}
+
+function showWinner(x) {
+    var winner;
+    var score;
+    if (x === 1) {
+        winner = "X wins!"
+        score = "show-x"
+    }
+    else if (x === 2) {
+        winner = "O wins!"
+        score = "show-o"
+    }
+    else if (x === 0) {
+        winner = "Draw!";
+    }
+
+    if (x != 0) {
+        if (document.getElementById(score).innerText === "-") {
+            document.getElementById(score).innerText = 1;
+        }
+        else {
+            document.getElementById(score).innerText = parseInt(document.getElementById(score).innerText) + 1;
+        }
+    }
+    document.getElementById("winner").innerText = winner;
+    document.getElementById("result").style.display = "flex";
+}
+
+function reset() {
+    var c0 = area[0].innerText;
+    var c1 = area[1].innerText;
+    var c2 = area[2].innerText;
+    var c3 = area[3].innerText;
+    var c4 = area[4].innerText;
+    var c5 = area[5].innerText;
+    var c6 = area[6].innerText;
+    var c7 = area[7].innerText;
+    var c8 = area[8].innerText;
+
+    for (var i = 0; i < area.length; i++) {
+        area[i].innerHTML = '<div class="divider"></div>';
+    }
 }
